02ed54bca1433d105563d0b4c753a4968c95ca0e
Daniel Fryar <daniel.fryar@gmail.com>
2020-06-29 12:07:12 -0500

    extra example

--- js/loops.js
+++ js/loops.js
@@ -25,15 +25,15 @@
 //     }
 // } while (true) ;
 
-let x = 1;
-while (x <= 10) {
-    console.log(x);
-    x++;
-}
-
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
+// let x = 1;
+// while (x <= 10) {
+//     console.log(x);
+//     x++;
+// }
+//
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
 
 // for (let monkeys = 9; monkeys > 0; monkeys--) {
 //     console.log(monkeys + " little monkeys jumping on the bed");
@@ -43,11 +43,11 @@ for (let i = 1; i <= 10; i++) {
 //     console.log(" ")
 // }
 
-for (let i=1;i<=20;i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
+// for (let i=1;i<=20;i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 
 let multiply = function(a,b) {
     let answer = 0;
@@ -58,6 +58,16 @@ let multiply = function(a,b) {
 }
 
 console.log(multiply(6, 7));
+
+let addition = function(a,b) {
+    let answer = a;
+    for (let i = 0; i<b;i++) {
+        answer++;
+    }
+    return answer;
+}
+
+console.log(addition(3,4));
 
 console.log("We are done")
 
