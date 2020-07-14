d26a81e788152934bb6f4e5808dcb2c78a51719d
Daniel Fryar <daniel.fryar@gmail.com>
2020-07-14 16:00:22 -0500

    finished review problems

--- js/jsiiiReview.js
+++ js/jsiiiReview.js
@@ -3,10 +3,20 @@
 // Welcome to your JS 3rd Assessment Review. This will include some things from the previous assessments, as well as Array and Object manipulation. Below are some practice questions for you to get started.
 
 // TODO: Write a function named 'typeOfValue' that takes in a value and returns its type.
-// typeOfValue("hello") // "string"
-// typeOfValue(123) // "number"
-// typeOfValue([]) // "object"
-// typeOfValue([4,5,6]) // "object"
+const typeOfValue = function (value) {
+    if (Array.isArray(value)) {
+        // test if it is an array
+        return "array";
+    } else
+        return typeof value;
+}
+let a = [4, 5, 6];
+console.log(typeOfValue("hello")); // "string"
+console.log(typeOfValue(123)); // "number"
+console.log(typeOfValue({})); // "object"
+console.log(typeOfValue(a)); // "array"
+console.log(typeOfValue(true)); // "boolean"
+console.log(typeOfValue(a[2]));
 
 // TODO: Write a function 'isPositive' that takes in a number and returns true or false based on whether the input is positive.
 // isPositive(2) // true
@@ -14,13 +24,41 @@
 // isPositive(0) // false
 
 // TODO: Write a function 'removeElement' that takes in an array and a value, and returns an Array with the first instance of the indicated value removed.
-// removeElement([1,2,3,4,5], 4) // returns [1,2,3,5]
-// removeElement([2,2,4,4,5], 2) // returns [2,4,4,5]
-// removeElement([10], 10) // returns []
+const removeElement = function (arr, val) {
+    // let index = arr.indexOf(val);
+    // if (index > -1) {
+    //     arr.splice(index,1)
+    // }
+    // return arr;
+    let arr2 = [];
+    let found = false;
+    for (let i=0;i<arr.length;i++) {
+        if (arr[i] === val && !found) {
+            found = true;
+            continue;
+        } //else {
+        arr2.push(arr[i]);
+        // }
+    }
+    return arr2;
+}
+console.log(removeElement([1, 2, 3, 4, 5], 4)); // returns [1,2,3,5]
+console.log(removeElement([2, 2, 4, 4, 5], 4)); // returns [2,4,4,5]
+console.log(removeElement([10], 10)); // returns []
 
 // TODO: Write a function 'sumOfNumbers' that takes in an array of numbers and returns the sum of all values.
-// sumOfNumbers([1,2,3,4,5]) // 15
-// sumOfNumbers([-1, 15]) // 14
+const sumOfNumbers = function (arr) {
+    let sum = 0;
+    // for (let i=0;i<arr.length;i++){
+    //     sum += arr[i];
+    // }
+    arr.forEach(function(item){
+        sum += item;
+    })
+    return sum;
+}
+console.log(sumOfNumbers([1, 2, 3, 4, 5])); // 15
+console.log(sumOfNumbers([-1, 15])); // 14
 
 // TODO: Write a function 'evensIndex' that takes in an array of numbers and returns an array containing the index values of all even numbers.
 // evensIndex([1,2,3,4,5,6]) // returns [1,3,5]
@@ -33,26 +71,34 @@
 //
 // stringToArray("1,2,3") // returns ['1','2','3']
 // stringToArray("dog") // returns ['dog']
-// Given the following array of objects, write a function 'findUsers' that returns an Array of usernames.
-//     var myMac = {};
-// myMac.users = [
-//     {
-//         username: "sophie",
-//         id: 1
-//     },
-//     {
-//         username: "vivian",
-//         id: 2
-//     },
-//     {
-//         username: "david",
-//         id: 3
-//     }
-// ];
-//
-// findUsers(myMac.users) // returns ['sophie','vivian','david']
 
-// TODO: Write a function 'addRole' that accepts the array of objects, then adds the property 'role' to each object with a value of 'instructor.' The results should look like this:
+// TODO: Given the following array of objects, write a function 'findUsers' that returns an Array of usernames.
+var myMac = {};
+myMac.users = [
+    {
+        username: "sophie",
+        id: 1
+    },
+    {
+        username: "vivian",
+        id: 2
+    },
+    {
+        username: "david",
+        id: 3
+    }
+];
+const findUsers = function (usersArray) {
+    let usernames = [];
+    for (let i=0;i<usersArray.length;i++) {
+        usernames.push(usersArray[i].username);
+    }
+    return usernames;
+}
+//
+console.log(findUsers(myMac.users)); // returns ['sophie','vivian','david']
+
+// TODO: Write a function 'addRole' that accepts an array of objects, then adds the property 'role' to each object with a value of 'instructor.' The results should look like this:
 // addRole("instructor");
 //
 // [
@@ -73,13 +119,41 @@
 //     }
 // ];
 //
+const addRole = function(role) {
+    // for (var i=0;i<myMac.users.length;i++) {
+    //     myMac.users[i].role = role;
+    // }
+    myMac.users.forEach(function(item){
+        item.role = role;
+    })
+}
+
+console.log(myMac.users);
+addRole("instructor");
+console.log(myMac.users);
 
 // TODO: Write a function 'countLetters' that takes in a string and a character, and counts the number of instances of that character in the string.
-// countLetters("banana", "a") // returns 3
-// countLetters("Bob", "b") // returns 2
-// countLetters("javascript", "x") // returns 0
+const countLetters = function(str,letter) {
+    let num = 0;
+    for (let i=0;i<str.length;i++) {
+        if (str[i].toLowerCase() === letter.toLowerCase()) {
+            num++;
+        }
+    }
+    return num;
+}
+console.log(countLetters("banana", "a")); // returns 3
+console.log(countLetters("Bob", "b")); // returns 2
+console.log(countLetters("javascript", "x")); // returns 0
 // ####BONUS:
 //
 // TODO: Write a function 'countAll' that takes in an array of strings and returns an array with the count of a character for each string. You may be able to use your countLetters function here.
-// countAll(["banana", "html", "java"], "a") // returns [3,0,2]
-// countAll(["push", "your", "commits"], "u") // returns [1,1,0]
+const countAll = function (strings, letter) {
+    let answer = [];
+    for (let i=0;i<strings.length;i++) {
+        answer.push(countLetters(strings[i], letter));
+    }
+    return answer;
+}
+console.log(countAll(["banana", "html", "java"], "a")); // returns [3,0,2]
+console.log(countAll(["push", "your", "commits"], "u")); // returns [1,1,0]
